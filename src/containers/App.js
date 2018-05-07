d766ed0329e511aa2f34164a4fd54c6c9c326d75
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-07 18:58:51 -0400

    Restructure the whole app.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import Search from './Search'
-import ListBooks from './ListBooks'
+import * as BooksAPI from '../utils/BooksAPI'
+import Search from '../components/Search'
+import ListBooks from '../components/ListBooks'
 import { Route } from 'react-router-dom'
-import './App.css'
+import '../styles/App.css'
 
 class BooksApp extends React.Component {
   state = {
