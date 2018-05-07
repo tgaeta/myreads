13e1e7d3b8cdcb6d38af1c902d93434226ac4c36
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-06 20:35:22 -0400

    Begin development on returning search results.

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
 
 class Search extends Component {
   state = {
     query: '',
     results: [],
+    view: 'search',
   }
 
   updateQuery = (query) => {
@@ -14,28 +17,42 @@ class Search extends Component {
     }))
   }
 
+  componentDidMount() {
+    BooksAPI.search('manage')
+      .then((results) => {
+        this.setState(() => ({
+          results
+        }))
+      })
+  }
+
   render() {
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to='/' className="close-search">Close</Link>
-          <div className="search-books-input-wrapper">
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link to='/' className='close-search'>Close</Link>
+          <div className='search-books-input-wrapper'>
             <input 
-              type="text" 
-              placeholder="Search by title or author"
+              type='text' 
+              placeholder='Search by title or author'
               value={this.state.query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            <BookShelf status='Search Results' books={this.state.query} />
+        <div className='search-books-results'>
+          <ol className='books-grid'>
+            <Book books={this.state.results} view='search' />
           </ol>
         </div>
       </div>
     );
   }
+
+  static propTypes = {
+    view: PropTypes.string.isRequired,
+  }
 }
+
 
 export default Search
