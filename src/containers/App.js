8f3f802548cd57a52bfd5c6d3a7255cffa3d0b70
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-10 09:24:21 -0400

    Display books on right bookshelf.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,75 @@
 import React from 'react'
 import * as BooksAPI from '../utils/BooksAPI'
 import Search from '../components/Search'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import '../styles/App.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import BookShelf from '../components/BookShelf'
 
 class BooksApp extends React.Component {
   state = {
     books: [],
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
   }
-  
+
   componentDidMount() {
-    BooksAPI.getAll()
+    this.getAllBooks()
+  }
+
+  getAllBooks = () => {
+    BooksAPI
+      .getAll()
       .then((books) => {
-        this.setState(() => ({
-          books
-        }))
+        this.setState(() => ({books}), () => this.setBooksToShelf(this.state.books))
       })
   }
 
-  render() {
-    const readingStatus = [
-      {status: 'Currently Reading'},
-      {status: 'Want to Read'},
-      {status: 'Read'},
-    ]
-    
-    const BookShelfElement =
-      readingStatus.map((book, i) => (
-        <BookShelf key={i} bookshelfTitle={book.status} books={this.state.books} />
-      ))
+  setBooksToShelf = (books) => {
+    let currently = []
+    let want = [] 
+    let reading = []
+    const cases = {
+      currentlyReading: (book) => currently.push(book),
+      wantToRead: (book) => want.push(book),
+      read: (book) => reading.push(book)
+    }
+    // use forEach since you aren't returning values from your map iterations
+    books.forEach(book => {
+      const updateArray = cases[book.shelf]
+      if (updateArray) {
+        updateArray(book)
+      } 
+   })
 
+    this.setState(() => ({currentlyReading: currently, wantToRead: want, read: reading}))
+  }
+
+  render() {
     return (
       <div className='app'>
-        <Route path='/search' render={() => (
-          <Search />
-        )} />    
-        <Route exact path='/' render={() => (
+        <Route path='/search' render={() => (<Search/>)}/>
+        <Route
+          exact
+          path='/'
+          render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
               <div>
-                {BookShelfElement}
+                <BookShelf bookshelfTitle='Currently Reading' books={this.state.currentlyReading}/>
+                <BookShelf bookshelfTitle='Want to Read' books={this.state.wantToRead}/>
+                <BookShelf bookshelfTitle='Read' books={this.state.read}/>
               </div>
             </div>
             <div className="open-search">
               <Link to='/search'>Add a book</Link>
             </div>
           </div>
-        )} />
+        )}/>
       </div>
     )
   }
