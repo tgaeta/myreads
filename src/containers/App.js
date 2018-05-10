93d6efdf74cae00fcf95132f1540c8e74bc1fafe
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-09 20:38:05 -0400

    Remove the ListBooks component.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import * as BooksAPI from '../utils/BooksAPI'
 import Search from '../components/Search'
-import ListBooks from '../components/ListBooks'
 import { Route } from 'react-router-dom'
 import '../styles/App.css'
+import { Link } from 'react-router-dom'
+import BookShelf from '../components/BookShelf'
 
 class BooksApp extends React.Component {
   state = {
     books: [],
   }
-
+  
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
@@ -19,22 +20,37 @@ class BooksApp extends React.Component {
       })
   }
 
-  removeBook = (book) => {
-    // logic to set a book to 'none'
-  }
-
-  updateBook = (book) => {
-    // logic to set a book to 'want to read', 'currently reading', and 'read'
-  }
-
   render() {
+    const readingStatus = [
+      {status: 'Currently Reading'},
+      {status: 'Want to Read'},
+      {status: 'Read'},
+    ]
+    
+    const BookShelfElement =
+      readingStatus.map((book, i) => (
+        <BookShelf key={i} bookshelfTitle={book.status} books={this.state.books} />
+      ))
+
     return (
       <div className='app'>
         <Route path='/search' render={() => (
           <Search />
         )} />    
         <Route exact path='/' render={() => (
-          <ListBooks books={this.state.books} />
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                {BookShelfElement}
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to='/search'>Add a book</Link>
+            </div>
+          </div>
         )} />
       </div>
     )
