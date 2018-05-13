bf7fc555ab37b432a6dc26b37d2ed848c1eeb8a6
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-13 10:45:02 -0400

    Clean up app and book.

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,37 +6,44 @@ const Book = props => {
   const { books, onChangeShelf } = props
 
   let book
-  if (books) {  
-    book = books.map((book) =>
+  if (books) {
+    book = books.map(book => (
       <li key={book.id}>
         <div className='book'>
           <div className='book-top'>
-          {book.imageLinks === undefined ? (
-            <div className='book-cover' style={{ width: 128, height: 193 }}></div>
-          ) : (
-            <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-          )}
-            <BookShelfChanger book={book} onChangeShelf={onChangeShelf} activeShelf={book.shelf || 'none'} />
+            {book.imageLinks === undefined ? (
+              <div className='book-cover' style={{ width: 128, height: 193 }} />
+            ) : (
+              <div
+                className='book-cover'
+                style={{
+                  width: 128,
+                  height: 193,
+                  backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                }}
+              />
+            )}
+            <BookShelfChanger
+              book={book}
+              onChangeShelf={onChangeShelf}
+              activeShelf={book.shelf || 'none'}
+            />
           </div>
           <div className='book-title'>{book.title}</div>
-            <div className='book-authors'>{book.authors}</div>
+          <div className='book-authors'>{book.authors}</div>
         </div>
       </li>
-    )
+    ))
   } else {
-    book = (<p> No results </p>)
+    book = <p> No results </p>
   }
 
-  return (
-    <ol className='books-grid'>
-      {book}
-    </ol>
-  )
+  return <ol className='books-grid'>{book}</ol>
 }
 
 Book.propTypes = {
   books: PropTypes.array.isRequired,
-  onChangeShelf: PropTypes.func.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
 }
 
 export default Book
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import * as BooksAPI from '../utils/BooksAPI'
+import { Route, Link } from 'react-router-dom'
 import Search from '../components/Search'
-import {Route} from 'react-router-dom'
-import '../styles/App.css'
-import {Link} from 'react-router-dom'
 import BookShelf from '../components/BookShelf'
+import * as BooksAPI from '../utils/BooksAPI'
+import '../styles/App.css'
 
 class BooksApp extends React.Component {
   state = {
@@ -21,74 +20,90 @@ class BooksApp extends React.Component {
   changeShelf = (book, shelf) => {
     if (this.state.books) {
       BooksAPI.update(book, shelf).then(() => {
-        book.shelf = shelf;
+        book.shelf = shelf
         this.setState(state => ({
-          books: state.books.filter(b => b.id !== book.id).concat([ book ])
+          books: state.books.filter(b => b.id !== book.id).concat([book])
         })),
-        this.setBooksToShelf(this.state.books)
+          this.setBooksToShelf(this.state.books)
       })
     }
   }
 
   getAllBooks = () => {
-    BooksAPI
-      .getAll()
-      .then((books) => {
-        this.setState(() => ({books}), () => this.setBooksToShelf(this.state.books))
-      })
+    BooksAPI.getAll().then(books => {
+      this.setState(
+        () => ({ books }),
+        () => this.setBooksToShelf(this.state.books)
+      )
+    })
   }
 
-  setBooksToShelf = (books) => {
+  setBooksToShelf = books => {
     let currently = []
-    let want = [] 
+    let want = []
     let reading = []
     const cases = {
-      currentlyReading: (book) => currently.push(book),
-      wantToRead: (book) => want.push(book),
-      read: (book) => reading.push(book)
+      currentlyReading: book => currently.push(book),
+      wantToRead: book => want.push(book),
+      read: book => reading.push(book)
     }
     // use forEach since you aren't returning values from your map iterations
     books.forEach(book => {
       const updateArray = cases[book.shelf]
       if (updateArray) {
         updateArray(book)
-      } 
-   })
+      }
+    })
 
-    this.setState(() => ({currentlyReading: currently, wantToRead: want, read: reading}))
+    this.setState(() => ({
+      currentlyReading: currently,
+      wantToRead: want,
+      read: reading
+    }))
   }
 
   render() {
     return (
-      <div className='app'>
-        <Route path='/search' render={() => (
-          <Search books={this.state.books} onChangeShelf={this.changeShelf}/>)}/>
-        <Route exact path='/'render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf
-                  bookshelfTitle='Currently Reading' 
-                  onChangeShelf={this.changeShelf} 
-                  books={this.state.currentlyReading}/>
-                <BookShelf
-                  bookshelfTitle='Want to Read' 
-                  onChangeShelf={this.changeShelf} 
-                  books={this.state.wantToRead}/>
-                <BookShelf
-                  bookshelfTitle='Read' 
-                  onChangeShelf={this.changeShelf} 
-                  books={this.state.read}/>
+      <div className="app">
+        <Route
+          path="/search"
+          render={() => (
+            <Search books={this.state.books} onChangeShelf={this.changeShelf} />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <BookShelf
+                    bookshelfTitle="Currently Reading"
+                    onChangeShelf={this.changeShelf}
+                    books={this.state.currentlyReading}
+                  />
+                  <BookShelf
+                    bookshelfTitle="Want to Read"
+                    onChangeShelf={this.changeShelf}
+                    books={this.state.wantToRead}
+                  />
+                  <BookShelf
+                    bookshelfTitle="Read"
+                    onChangeShelf={this.changeShelf}
+                    books={this.state.read}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">Add a book</Link>
               </div>
             </div>
-            <div className="open-search">
-              <Link to='/search'>Add a book</Link>
-            </div>
-          </div>
-        )}/>
+          )}
+        />
       </div>
     )
   }
