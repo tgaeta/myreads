fe419187ce28636d97edb2444e0d4794ebd0470d
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-13 09:58:07 -0400

    Convert to stateless functional components.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 
-class Book extends Component {
-  render() {
-    console.log(this.props)
-    const { books, onChangeShelf } = this.props
+const Book = props => {
+  const { books, onChangeShelf } = props
 
-    let book
-    if (books) {  
-      book = books.map((book) =>
-        <li key={book.id}>
-          <div className='book'>
-            <div className='book-top'>
-            {book.imageLinks === undefined ? (
-              <div className='book-cover' style={{ width: 128, height: 193 }}></div>
-            ) : (
-              <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-            )}
-              <BookShelfChanger book={book} onChangeShelf={onChangeShelf} activeShelf={book.shelf || 'none'} />
-            </div>
-            <div className='book-title'>{book.title}</div>
-              <div className='book-authors'>{book.authors}</div>
+  let book
+  if (books) {  
+    book = books.map((book) =>
+      <li key={book.id}>
+        <div className='book'>
+          <div className='book-top'>
+          {book.imageLinks === undefined ? (
+            <div className='book-cover' style={{ width: 128, height: 193 }}></div>
+          ) : (
+            <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          )}
+            <BookShelfChanger book={book} onChangeShelf={onChangeShelf} activeShelf={book.shelf || 'none'} />
           </div>
-        </li>
-      )
-    } else {
-      book = (<p> No results </p>)
-    }
-
-    return (
-      <ol className='books-grid'>
-        {book}
-      </ol>
-    );
+          <div className='book-title'>{book.title}</div>
+            <div className='book-authors'>{book.authors}</div>
+        </div>
+      </li>
+    )
+  } else {
+    book = (<p> No results </p>)
   }
+  return (
+    <ol className='books-grid'>
+      {book}
+    </ol>
+  )
+
 }
 
 export default Book
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class BookShelf extends Component {
-  render() {
-    const { books, bookshelfTitle, onChangeShelf } = this.props
-    
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{bookshelfTitle}</h2>
-        <div className="bookshelf-books">
-          <Book books={books} onChangeShelf={onChangeShelf}/>
-        </div>
+const BookShelf = (props) => {
+  const { books, bookshelfTitle, onChangeShelf } = props
+  
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{bookshelfTitle}</h2>
+      <div className="bookshelf-books">
+        <Book books={books} onChangeShelf={onChangeShelf}/>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default BookShelf
