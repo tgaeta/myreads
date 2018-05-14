b8563d877737bec1abdf47935e5fb1732d53b93a
Tony Gaeta <tony.gaeta@gmail.com>
2018-05-14 08:32:34 -0400

    Convert to a stateless component.

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class BookShelfChanger extends Component {
-  updateBook(shelf) {
-    this.props.onChangeShelf(this.props.book, shelf)
+const BookShelfChanger = props => {
+  const { book, onChangeShelf, activeShelf } = props
+  
+  const updateBook = shelf => {
+    onChangeShelf(book, shelf)
   }
 
-  render() {
-    const { activeShelf } = this.props
-    return (
-      <div className='book-shelf-changer'>
-        <select defaultValue={activeShelf} onChange={e => this.updateBook(e.target.value)}>
-          <option value='moveTo' disabled>Move to...</option>
-          <option value='currentlyReading'>Currently Reading</option>
-          <option value='wantToRead'>Want to Read</option>
-          <option value='read'>Read</option>
-          <option value='none'>None</option>
-        </select>
-      </div>
-    )
-  }
+  return (
+    <div className='book-shelf-changer'>
+      <select defaultValue={activeShelf} onChange={e => updateBook(e.target.value)}>
+        <option value='moveTo' disabled>Move to...</option>
+        <option value='currentlyReading'>Currently Reading</option>
+        <option value='wantToRead'>Want to Read</option>
+        <option value='read'>Read</option>
+        <option value='none'>None</option>
+      </select>
+    </div>
+  )
 }
 
 BookShelfChanger.propTypes = {
